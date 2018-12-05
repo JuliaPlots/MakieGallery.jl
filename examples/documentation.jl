2364e031ffc2ff92793283ece54dc1f89a604979
Ian Butterworth <i.r.butterworth@gmail.com>
2018-12-05 10:42:02 -0500

    Corrected bug in colormaps example
    As discussed in https://github.com/JuliaPlots/Makie.jl/issues/255

--- examples/documentation.jl
+++ examples/documentation.jl
@@ -101,7 +101,7 @@
                 scene,
                 range(0, stop = 10, length = length(c)),
                 range(0, stop = 1, length = length(c)),
-                reshape(c, (1, length(c))),
+                reshape(c, (length(c),1)),
                 show_axis = false
             )[end]
             text!(
@@ -110,7 +110,7 @@
                 position = Point2f0(-0.1, 0.5 + h),
                 align = (:right, :center),
                 show_axis = false,
-                textsize = 0.4
+                textsize = 1
             )
             translate!(cbar, 0, h, 0)
             h -= (1 + offset)
