89fd0b3434bfcd44a131709a956e9db39765bd12
asinghvi17 <anshulsinghvi@gmail.com>
2019-06-19 17:52:43 +0500

    reorganize upggrade_recordings

--- test/upgrade_recordings.jl
+++ test/upgrade_recordings.jl
@@ -1,5 +1,7 @@
 repo = joinpath(homedir(), "ReferenceImages", "gallery")
-MakieGallery.generate_thumbnails(repo)
 
 recordings = joinpath(@__DIR__, "test_recordings")
+
 cp(recordings, repo, force = true)
+
+MakieGallery.generate_thumbnails(repo)
