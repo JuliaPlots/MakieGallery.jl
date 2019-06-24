0949e6b45a801e5ea309d6fb92ff58dae927839f
asinghvi17 <anshulsinghvi@gmail.com>
2019-06-24 07:24:41 +0500

    transfer interactive diffeq

--- examples/interactive.jl
+++ examples/interactive.jl
@@ -539,57 +539,6 @@
         RecordEvents(scene, @replace_with_a_path)
     end
 
-    @cell "Interactive Differential Equation" [lines, slider, animated, record, diffeq, interactive] begin
-
-        using DifferentialEquations, ParameterizedFunctions
-        import AbstractPlotting: textslider
-        lorenz = @ode_def Lorenz begin           # define the system
-            dx = σ * (y - x)
-            dy = x * (ρ - z) - y
-            dz = x * y - β*z
-        end σ ρ β
-
-        u0 = [1.0,0.0,0.0]                       # initial conditions
-        tspan0 = (0.0,100.0)                      # initial timespan
-        p0 = [10.0,28.0,8/3]                      # initial parameters
-        prob = ODEProblem(lorenz, u0, tspan0, p0)  # define the problem
-
-        ## setup sliders and plotting
-
-        "The order of magnitude to range between."
-        OME = 8
-
-        sσ, oσ = textslider(exp10.(-OME:0.001:OME), "σ", start = p0[1]);
-
-        sρ, oρ = textslider(exp10.(-OME:0.001:OME), "ρ", start = p0[2]);
-
-        sβ, oβ = textslider(exp10.(-OME:0.001:OME), "β", start = p0[3]);
-
-        st, ot = textslider(exp10.(-OME:0.001:OME), "tₘₐₓ", start = tspan0[end]);
-
-        sr, or = textslider(100:10000, "resolution", start = 2000);
-
-        trange = lift(ot, or) do tmax, resolution
-            LinRange(0.0, tmax, resolution)
-        end
-
-        data = lift(oσ, oρ, oβ, trange) do σ, ρ, β, ts
-            Point3f0.(
-                solve(
-                    remake(
-                        prob;
-                        p = [σ, ρ, β],
-                        tspan = (ts[1], ts[end])
-                    )
-                )(ts).u
-            )  # change to fit the dimensionality - maybe even return 2 arrays, or a set of `Point2`s...
-        end
-        parent = Scene(resolution = (1000, 500))
-        three = lines(data, linewidth = 1, transparency = true, color = ("#fe4a49", 0.6))
-        meshscatter!(three, data, markersize = 0.1, color = :gray)
-        scene = vbox(hbox(sσ, sρ, sβ, st, sr), three, parent = parent)
-        RecordEvents(scene, @replace_with_a_path)
-    end
 end
 
 function record_example_events()
