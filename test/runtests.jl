8ca12ec1d499a6b165522c5617e4191f4eaebcca
SimonDanisch <sdanisch@gmail.com>
2019-06-25 17:12:19 +0200

    update image + test length

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Makie, AbstractPlotting
 using Statistics
 
 database = (get(ENV, "MAKIEGALLERY_MINIMAL", "false") == "true") ? MakieGallery.load_tests() : MakieGallery.load_database()
-    
+
 # THese examples download additional data - don't want to deal with that!
 # to_skip = ["WorldClim visualization", "Image on Geometry (Moon)", "Image on Geometry (Earth)"]
 # # we directly modify the database, which seems easiest for now
@@ -24,6 +24,7 @@ mkpath(test_record_path)
 # ]
 # filter!(x-> x.title in rerecord, database)
 examples = MakieGallery.record_examples(test_record_path)
+@test length(examples) == length(database)
 # MakieGallery.generate_preview(test_record_path, joinpath(homedir(), "Desktop", "index.html"))
 # MakieGallery.generate_thumbnails(test_record_path)
 # MakieGallery.gallery_from_recordings(test_record_path, joinpath(test_record_path, "index.html"))
