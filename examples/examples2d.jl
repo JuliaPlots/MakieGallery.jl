87baa0d3fe59592adfc14750fb73c429e4bde3da
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-08-02 13:55:28 +0530

    Add plot transform example

--- examples/examples2d.jl
+++ examples/examples2d.jl
@@ -645,3 +645,41 @@ end
     end
 
 end
+
+@block AnshulSinghvi ["Transformations"] begin
+    @cell "Transforming lines" [transformation, lines] begin
+        N = 7 # number of colours in default palette
+        sc = Scene()
+
+        xs = 0:9        # data
+        ys = zeros(10)
+
+        for i in 1:N    # plot lines
+            lines!(sc,
+                xs, ys;
+                color = AbstractPlotting.default_palettes.color[][i],
+                limits = FRect((0, 0), (10, 10)),
+                linewidth = 5
+            ) # plot lines with colors
+        end
+
+        center!(sc)
+
+        @substep
+
+        for (i, rot) in enumerate(LinRange(0, π/2, N))
+            rotate!(sc.plots[i+1], rot)
+            arc!(sc,
+                Point2f0(0),
+                (8 - i),
+                pi/2,
+                (pi/2-rot);
+                color = sc.plots[i+1].color,
+                linewidth = 5,
+                linestyle = :dash
+            )
+        end
+
+        center!(sc)
+    end
+end
