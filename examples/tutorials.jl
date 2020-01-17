034f0eba4abde74f3ccd5c59cd44a10e93b79f9a
akshat <akshatmehrotra2004@gmail.com>
2020-01-17 20:54:04 +0530

    Add the Attribute Usage tutorial to tutorial.jl

--- examples/tutorials.jl
+++ examples/tutorials.jl
@@ -35,7 +35,7 @@
         lines!(scene, x, y2, color = :black)
         scatter!(scene, x, y2, color = :green, marker = :utriangle, markersize = 0.1)
     end
-    
+
     @cell "Tutorial removing from a scene" [tutorial, line, scene, markers] begin
         x = range(0, stop = 2pi, length = 80)
         f1(x) = sin.(x)
@@ -134,4 +134,105 @@
     #
     # end
 
+end
+@block "AkshatMehrotra" [tutorials] begin
+    @cell "attribute usage : shading" [tutorial, mesh] begin
+        using GeometryTypes
+            scene = mesh(
+                HyperRectangle(Vec3f0(1, 1, 1), Vec3f0(1, 1, 3)),
+                color = (:green, 0.4),
+                show_axis = false,
+                shading = false,
+                center = false,
+            )
+            scene
+    end
+    @cell "attribute usage : colormap, color, colorrange, linestyle, linewidth, align, pos, textsize, rotation" [tutorial, lines, text] begin
+
+        scene = Scene()
+        y = 200 * rand(300)
+        x = range(0, 600, length = 300)
+        c = range(1, stop = 0, length = 600)
+        lines!(
+            scene,
+            x,
+            y,
+            colormap = :BuPu,
+            color = c,
+            colorrange=(1,0.5),
+            linestyle = :dashdotdot,
+            linewidth = 3.0,
+        )
+        text!(
+            scene,
+            "Red Dead Redemption 2 is the best game ever!",
+            align = (:left, :center),
+            position = (0, -100),
+            color = :red,
+            font = "Blackchancery",
+            textsize = 70,
+            rotation = pi / 6,
+        )
+        scene
+    end
+    @cell "attribute usage : markersize, strokecolor, strokewidth, glowcolor, glowwidth, marker, marker offset" [tutorial, scatter] begin
+        scene = Scene()
+        points = Point2f0[(i, i) for i = 1:6]
+        offset = rand(Point2f0, length(points)) ./ 5
+        scatter!(
+            scene,
+            points,
+            color = :gray,
+            markersize = 0.2,
+            strokecolor = :pink,
+            strokewidth = 8,
+            marker = :x,
+        )
+        scatter!(
+            scene,
+            points,
+            glowcolor = :red,
+            color = :black,
+            marker_offset = offset,
+            markersize = 1,
+            glowwidth = 5.0,
+            marker = :+,
+        )
+        scene
+    end
+    @cell "attributes usage : visible" [tutorial, scatter, text]
+        scene = Scene()
+        y = rand(3)
+        scatter!(scene, y, visible = false)
+        text!(
+            scene,
+            "The above plot is not visible!",
+        )
+        scene
+    end
+    @cell "attribute usage : interpolate" [tutorial, heatmap, image]
+        source_img = load(download("https://images-na.ssl-images-amazon.com/images/I/41OEdd1En8L._SX322_BO1,204,203,200_.jpg"))
+        img = Float32.(channelview(Gray.(source_img)))
+        heatmap!(scene, img, interpolate = true)
+        scene
+    end
+    @cell "attribute usage : absorption, algorithm" [tutorial, volume]
+        scene = Scene()
+        volume!(scene, rand(50, 50, 50), algorithm = :absorption, absorption = 7.0f0)
+        scene
+    end
+    @cell "attribute usage : isorange, isovalue" [tutorial, volume]
+        scene = Scene()
+        img = Float32.(channelview(source_img))
+        volume!(scene, img, isorange = 0.012f0, isovalue = 0.3f0)
+        scene
+    end
+    @cell "attribute usage : levels" [contour]
+        N = 20
+        x = range(-0.5, stop = 0.5, length = N)
+        y = range(-0.5, stop = 0.5, length = N)
+        z = x .* y'
+        a = contour(x, y, z, levels = 100, colormap = :magma, fillrange = true)
+        a
+    end
 end
