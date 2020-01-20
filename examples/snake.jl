5309f8f4b888cc19dc53c2632df0862825658587
Simon <sdanisch@gmail.com>
2020-01-20 14:54:40 +0100

    Update snake.jl
    * use Point2f0 instead of treating x & y separately
    * update plots inplace instead of creating new plots over and over again (much, much more efficient)
    * use on instead of lift (since we don't actually care about the result)
    * use 1 element arrays as global references (could also use Base.Ref) ... Should make globals const, but it's a bit annoying for interactive use
    *

--- examples/snake.jl
+++ examples/snake.jl
@@ -1,6 +1,5 @@
 using Makie
 
-score = 0
 println("  ______   __    _       __       _   _   _______")
 println("/ ______| |  \\  | |     /  \\     | | / / |  _____|")
 println("| |____   | \\ \\ | |    / /\\ \\    | |/ /  | |____")
@@ -10,76 +9,66 @@ println("|_______/ |_|  \\__| /_/      \\_\\ |_| \\_\\ |_______|")
 println("")
 println("use the arrow keys to move and collect the red fruit. The snake gets faster and faster as the game progresses!")
 scene = Scene(resolution = (400, 400), raw = true, camera = campixel!)
-scene = scatter!([10], [10], color = :green, marker = :rect, markersize = 20)
-display(scene)
 
-k = [[0, 20], [0, 20], [0, 20], [0, 20]]
-dir = lift(scene.events.keyboardbuttons) do but
-    
+k = Point2f0[(0, 20), (0, 20), (0, 20), (0, 20)]
+
+on(scene.events.keyboardbuttons) do but
     if ispressed(but, Keyboard.left)
-        global k[1] = [-20, 0]
-
+        k[1] = (-20, 0)
     elseif ispressed(but, Keyboard.up)
-        global k[1] = [0, 20]
-        
+        k[1] = (0, 20)
     elseif ispressed(but, Keyboard.right)
-        global k[1] = [20, 0]
-        
+        k[1] = (20, 0)
     elseif ispressed(but, Keyboard.down)
-        global k[1] = [0, -20]
-        
+        k[1] = (0, -20)
     end
-    
 end
 
-y = [70, 50, 30, 10]
-x = [10, 10, 10, 10]
-fruit_x = 90
-fruit_y = 110
-game_over = false
-speed = 0.3
-while game_over == false
-    
-    scene = scatter!(x, y, color = :white, marker = :rect, markersize = 25)
-    scene = scatter!([fruit_x], [fruit_y], color = :white, marker = :rect, markersize = 25)
+xy = Point2f0.(10, [70, 50, 30, 10])
+fruit = [Point2f0(90, 110)]
+game_over = [false]
+speed = [0.3]
+score = [0]
 
-    for i1 = 0:(length(x) - 2)
-        n1 = length(x) - i1
-        if k[n1 - 1] == [(x[n1 - 1] - x[n1]), (y[n1 - 1] - y[n1])]
+snake = scatter!(xy, color = :white, marker = :rect, markersize = 25)[end]
+fruit_plot = scatter!(fruit, color = :white, marker = :rect, markersize = 25)[end]
+
+display(scene)
+
+while game_over[] == false && isopen(scene)
+
+    for i1 in 0:(length(xy) - 2)
+        n1 = length(xy) - i1
+        if k[n1 - 1] == (xy[n1 - 1] - xy[n1])
             k[n1] = k[n1 - 1]
         end
     end
-    
-    
 
-    for f = 2:length(x)
-        if x[f] == x[1] && y[f] == y[1]
-            global game_over = true
+    for f in 2:length(xy)
+        if xy[f] == xy[1]
+            game_over[] = true
         end
     end
     
-    if x[1] < 10 || x[1] > 390 || y[1] < 10 || y[1] > 390
-        global game_over = true
+    if xy[1][1] < 10 || xy[1][1] > 390 || xy[1][2] < 10 || xy[1][2] > 390
+        game_over[] = true
     end
 
-    for i = 1:length(x)
-        x[i] += k[i][1]
-        y[i] += k[i][2]
+    for i in 1:length(xy)
+        xy[i] += k[i]
     end
-
-    if x[1] == fruit_x && y[1] == fruit_y
-        global fruit_x = rand([10, 30, 50, 70, 90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290, 310, 330, 350, 370, 390])
-        global fruit_y = rand([10, 30, 50, 70, 90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290, 310, 330, 350, 370, 390])
-        push!(x, x[end] - k[end][1])
-        push!(y, y[end] - k[end][2])
+    if xy[1] == fruit[]
+        fruit[] = Point2f0(rand(10:20:390), rand(10:20:390))
+        push!(xy, xy[end] - k[end])
         push!(k, k[end])
-        global speed -= 0.01
-        global score += 1
+        speed[] -= 0.01
+        score[] += 1
     end
-    scene = scatter!(x, y, color = :green, marker = :rect, markersize = 20)
-    scene = scatter!([fruit_x], [fruit_y], color = :red, marker = :rect, markersize = 20)
-    sleep(speed)
-
+    snake[1] = xy
+    snake.color = :green
+    fruit_plot.color = :red
+    fruit_plot[1] = fruit
+    sleep(speed[])
 end
 
 for i = 1:100
@@ -87,12 +76,4 @@ for i = 1:100
 end
 
 println("Game Over. Better Luck next time!")
-println("Your score was " * string(score) * ".")
-for i3 = 1:3
-    scene = scatter!(x, y, color = :white, marker = :rect, markersize = 25)
-    sleep(0.5)
-    scene = scatter!(x, y, color = :green, marker = :rect, markersize = 20)
-    sleep(0.5)
-end
-
-
+println("Your score was " * string(score[]) * ".")
