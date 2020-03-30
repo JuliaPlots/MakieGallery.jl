52ac05a3921b5ab55a3644d9708f3333273bdbf2
SimonDanisch <sdanisch@protonmail.com>
2020-03-30 19:52:30 +0200

    fix statsmakie

--- src/testruns.jl
+++ src/testruns.jl
@@ -1,6 +1,6 @@
 const makiegallery_dir = dirname(dirname(@__DIR__))
 
-const current_ref_version = Ref{String}("v0.3.0")
+const current_ref_version = Ref{String}("v0.3.1")
 
 """
     ref_image_dir(version = string(current_ref_version[]))
