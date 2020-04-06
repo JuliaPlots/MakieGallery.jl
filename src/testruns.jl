c8c9df2e9c5d9424511428208909656d5ff11e18
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-04-06 20:01:23 +0530

    Fix the band example and release (#244)
    * Bump refimage version
    
    * Release a new version
    
    * do it again

--- src/testruns.jl
+++ src/testruns.jl
@@ -1,6 +1,6 @@
 const makiegallery_dir = dirname(dirname(@__DIR__))
 
-const current_ref_version = Ref{String}("v0.3.2")
+const current_ref_version = Ref{String}("v0.3.4")
 
 """
     ref_image_dir(version = string(current_ref_version[]))
