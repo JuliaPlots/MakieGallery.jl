ac3d4bf5c3ea73ec97e226b0b0c444db878ae434
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-04-11 21:01:50 +0530

    try incorporating JuliaDynamics theme as a testbed

--- docs/make.jl
+++ docs/make.jl
@@ -279,6 +279,23 @@ end
 #                 Building HTML documentation with Documenter                  #
 ################################################################################
 
+@info("Compiling themes")
+
+
+for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
+    download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file", joinpath(@__DIR__, file))
+end
+# create the themes
+for w in ("light", "dark")
+    header = read(joinpath(@__DIR__, "juliadynamics-style.scss"), String)
+    theme = read(joinpath(@__DIR__, "juliadynamics-$(w)defs.scss"), String)
+    write(joinpath(@__DIR__, "juliadynamics-$(w).scss"), header*"\n"*theme)
+end
+# compile the themes
+Themes.compile(joinpath(@__DIR__, "juliadynamics-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
+Themes.compile(joinpath(@__DIR__, "juliadynamics-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
+
+
 @info("Running `makedocs` with Documenter.")
 
 makedocs(
@@ -288,7 +305,8 @@ makedocs(
         prettyurls = false,
         assets = [
             "assets/favicon.ico",
-            "assets/syntaxtheme.css"
+            "assets/syntaxtheme.css",
+            asset("https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap", class=:css)
         ],
     ),
     sitename = "Makie.jl",
