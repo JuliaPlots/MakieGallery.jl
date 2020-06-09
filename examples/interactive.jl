c30dc650a679df4cd9af27a8811cfc143d71a4a1
SimonDanisch <sdanisch@protonmail.com>
2020-06-09 15:27:16 +0200

    refactor test examples

--- examples/interactive.jl
+++ examples/interactive.jl
@@ -56,33 +56,6 @@
         RecordEvents(final, @replace_with_a_path)
     end
 
-    @cell "Mouse hover with MakieLayout" [interactive] begin
-        using AbstractPlotting
-        import MakieLayout
-        import MakieLayout: LAxis, GridLayout
-        scene, layout = let nrows=1, ncols=1
-            scene = Scene(; camera = AbstractPlotting.campixel!,
-                resolution = (800, 800),
-                backgroundcolor = :white)
-            gl = GridLayout(scene, nrows, ncols, alignmode = MakieLayout.Outside(30))
-            scene, gl
-        end
-        layout[1,1] = laxis = LAxis(scene, title="workspace")
-        mouseposition(laxis.scene)
-        mousemarker = lift(laxis.scene.events.mouseposition) do mp
-            area = pixelarea(laxis.scene)[]
-            mp = Point2f0(mp) .- minimum(area)
-            r = [Point2f0(AbstractPlotting.to_world(laxis.scene, mp))]
-            return r
-        end
-        AbstractPlotting.scatter!(laxis, mousemarker; markersize=3)
-        scene
-
-        # Do not execute beyond this point!
-
-        RecordEvents(scene, @replace_with_a_path)
-    end
-
     @cell "Mouse Picking" [scatter, heatmap, interactive] begin
         img = rand(100, 100)
         scene = Scene(resolution = (500, 500))
@@ -347,11 +320,11 @@
     @cell "Robot Arm" [slider, interactive, linesegments, vbox, "3d"] begin
 
         using AbstractPlotting
-        using AbstractPlotting: Mesh, Scene, LineSegments, translate!, rotate!, vbox, hbox, qrotation, mesh!
+        using AbstractPlotting: Mesh, Scene, LineSegments, translate!, rotate!
+        using AbstractPlotting: vbox, hbox, qrotation, mesh!
         using GeometryBasics: Vec3f0, Point3f0, Sphere
         using AbstractPlotting: textslider
         using Observables: on
-        using GLMakie
 
         """
           example by @pbouffard from JuliaPlots/Makie.jl#307
--- examples/makielayout.jl
+++ examples/makielayout.jl
@@ -1,43 +1,32 @@
-@block SimonDanisch [layout] begin
-
-    @cell "Layouting" [scatter, lines, surface, heatmap, vbox] begin
-        p1 = scatter(rand(10), markersize = 1)
-        p2 = lines(rand(10), rand(10))
-        p3 = surface(0..1, 0..1, rand(100, 100))
-        p4 = heatmap(rand(100, 100))
-        x = 0:0.1:10
-        p5 = lines(0:0.1:10, sin.(x))
-        pscene = vbox(
-            hbox(p1, p2),
-            p3,
-            hbox(p4, p5, sizes = [0.7, 0.3]),
-            sizes = [0.2, 0.6, 0.2]
-        )
-    end
-
-    @cell "Comparing contours, image, surfaces and heatmaps" [image, contour, surface, heatmap, vbox] begin
-        N = 20
-        x = LinRange(-0.3, 1, N)
-        y = LinRange(-1, 0.5, N)
-        z = x .* y'
-        hbox(
-            vbox(
-                contour(x, y, z, levels = 20, linewidth =3),
-                contour(x, y, z, levels = 0, linewidth = 0, fillrange = true),
-                heatmap(x, y, z),
-            ),
-            vbox(
-                image(x, y, z, colormap = :viridis),
-                surface(x, y, fill(0f0, N, N), color = z, shading = false),
-                image(-0.3..1, -1..0.5, AbstractPlotting.logo())
-            )
-        )
-    end
-end
-
 @block JuliusKrumbiegel ["makielayout", "layout", "2d"] begin
     using MakieLayout
 
+    @cell "Mouse hover with MakieLayout" [interactive] begin
+        using AbstractPlotting
+        import MakieLayout
+        import MakieLayout: LAxis, GridLayout
+        scene, layout = let nrows=1, ncols=1
+            scene = Scene(; camera = AbstractPlotting.campixel!,
+                resolution = (800, 800),
+                backgroundcolor = :white)
+            gl = GridLayout(scene, nrows, ncols, alignmode = MakieLayout.Outside(30))
+            scene, gl
+        end
+        layout[1,1] = laxis = LAxis(scene, title="workspace")
+        mouseposition(laxis.scene)
+        mousemarker = lift(laxis.scene.events.mouseposition) do mp
+            area = pixelarea(laxis.scene)[]
+            mp = Point2f0(mp) .- minimum(area)
+            r = [Point2f0(AbstractPlotting.to_world(laxis.scene, mp))]
+            return r
+        end
+        AbstractPlotting.scatter!(laxis, mousemarker; markersize=3)
+        scene
+
+        # Do not execute beyond this point!
+
+        RecordEvents(scene, @replace_with_a_path)
+    end
     @cell "Faceting" [faceting, grid] begin
         # layoutscene is a convenience function that creates a Scene and a GridLayout
         # that are already connected correctly and with Outside alignment
